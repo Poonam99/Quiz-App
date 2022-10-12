3ad5bd51031a117fdec34ec3dcfc2db5369e4292
Poonam99 <poonamtabassum9909@gmail.com>
2022-10-12 16:21:19 +0600

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../src/Layout/Main';
+import Home from '../src/component/Home/Home';
+import Errorsite from '../src/component/Errorsite/Errorsite';
+import Quiz from '../src/component/Quiz/Quiz';
+import Statistics from '../src/component/statistics/statistics';
+import Blog from '../src/component/Blogs/Blogs';
+
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      errorElement: <Errorsite></Errorsite>,
+      children: [{
+        path: '/',
+        loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+        element: <Home></Home>
+      },
+      {
+        path: '/home',
+        loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+        element: <Home></Home>
+      },
+      {
+        path: '/home/:id',
+        loader: ({ params }) => fetch(`https://openapi.programming-hero.com/api/quiz/${params.id}`),
+        element: <Quiz></Quiz>,
+      },
+      {
+        path: '/statistics',
+        loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+        element: <Statistics></Statistics>
+
+      },
+      {
+        path: '/blog',
+        element: <Blog></Blog>
+      }
+      ]
+
+
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
