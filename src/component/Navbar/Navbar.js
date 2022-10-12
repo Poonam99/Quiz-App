5f66833583dbae62a8bf7e13cb62a28ac3bf9911
Poonam99 <poonamtabassum9909@gmail.com>
2022-10-12 15:02:47 +0600

    first commit

---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import img from '../quiz img.jpg';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className='nav-container'>
+                <div className='title-container'>
+                    <img src={img} alt="" />
+                    <h1>Quiz Winners Team</h1>
+                </div>
+                <div className='route-container'>
+                    <NavLink to={'/home'}>Topic</NavLink>
+                    <NavLink to={'/Quiz-Topics'}>Quiz items</NavLink>
+                    <NavLink to={'/blog'}>Blog</NavLink>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
